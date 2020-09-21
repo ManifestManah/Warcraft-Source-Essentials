cf752181e0dae8fa7b13ae4dd2dad2f2cb0e5a6d
Manifest <christoffer.soendergaard@outlook.com>
2020-09-21 13:55:00 +0200

    Update commandsx.py
    Fixed an error (hopefully) that kept popping up in people's console.

--- addons/source-python/plugins/commandsx/commandsx.py
+++ addons/source-python/plugins/commandsx/commandsx.py
@@ -266,10 +266,9 @@ def headshot_immunity(command):
 @PreEvent('player_hurt')
 def pre_hurt(ev):
     victim = Player.from_userid(int(ev['userid']))
+    attacker = Player.from_userid(int(ev['attacker']))
     if ev['attacker'] > 1:
-        attacker = Player.from_userid(int(ev['attacker']))
         damage = int(ev['dmg_health'])
-        
         headshot_immunity = wcsgroup.getUser(victim.userid,'headshot_immunity')
         if headshot_immunity != None:
             if victim.hitgroup == HitGroup.HEAD:
