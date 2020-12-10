19aba8a0847bff1a098856bc2dca2f5e0d254873
Manifest <christoffer.soendergaard@outlook.com>
2020-12-10 19:42:06 +0100

    Fixed: es_delayed issues
    This should fix the issue that prevents wcs commands to be executed after es_delayed via the server console.
    
    (Thanks to Pwned for this solution)

--- addons/source-python/plugins/es_emulator/eventscripts/esc/cmds/run.py
+++ addons/source-python/plugins/es_emulator/eventscripts/esc/cmds/run.py
@@ -11,11 +11,11 @@ def commandv(argv):
   
 @Command(argsfrom=2, syntax='<seconds> <commandstring>', types=FLOAT, desc='Will run <commandstring>, after <seconds> seconds.')
 def delayed(argv, args):
-  gamethread.delayed(argv[0], stack.queueline, (argv[1], argv[2:], args, esc.stack.currentblock, True))
+  gamethread.delayed(argv[0], stack.queueline, (argv[1], argv[2:], None, esc.stack.currentblock, True))
 
-@Command(syntax='<commandstring>', desc='Adds a command to the end of the command queue.')
+@Command(argsfrom=1, syntax='<commandstring>', desc='Adds a command to the end of the command queue.')
 def soon(argv, args):
-  queue(stack.queueline, (argv[0], argv[1:], args, esc.stack.currentblock, True))
+  queue(stack.queueline, (argv[0], argv[1:], None, esc.stack.currentblock, True))
 
 @Command(syntax='<file>', desc='Runs an exec file from memory.')
 def old_mexec(argv):
